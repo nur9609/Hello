5ebc63e344251e10f22b2895474e6b18fa1d4c20
Nurbolat Azhgaliev <nur9609@gmail.com>
2020-03-02 16:57:31 +0600

    note is deleted

--- note/src/main/java/com/homework/note/NoteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.homework.note;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-interface NoteRepository extends JpaRepository<Note, Long> {
-}
